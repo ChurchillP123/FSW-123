edde742aa376bd6099569af38448e7ec7ca38c66
Churchill perry <churchill.perry@bryanuniversity.edu>
2021-12-09 23:16:47 -0600

    updated todo list

--- week3/Assignment2/comp-delete/src/ToDoList.js
+++ week3/Assignment2/comp-delete/src/ToDoList.js
@@ -1,9 +1,8 @@
 import Todo from './Todo.js';
 
 function ToDoList({todos, completeTodo, deleteTodo}) {
-    const data = Array.from({todos});
 
-    const todoList = data.map((todo) => <Todo todo ={todo} key ={todo.id} completeTodo ={completeTodo} deleteTodo ={deleteTodo}/>)
+    const todoList = todos["Todos"].map((todo) => <Todo todo ={todo} key ={todo.id} completeTodo ={completeTodo} deleteTodo ={deleteTodo}/>)
     return (
         <>
             <h1>List of Todos</h1>
--- week3/Assignment2/comp-delete/src/Todo.js
+++ week3/Assignment2/comp-delete/src/Todo.js
@@ -2,6 +2,7 @@ function Todo({todo, completeTodo, deleteTodo}) {
     return (
         <div className = "divItems">
             <input type="checkbox" checked ={todo.isCompleted} onChange ={() => completeTodo(todo.id)}/>
+            {todo.text}
             <button onClick ={() => deleteTodo(todo.id)}>X</button>
         </div>
     );
