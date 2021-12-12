87e2ee0d7fd9a2c7b1da10156fab21eeb5752dbb
Churchill perry <churchill.perry@bryanuniversity.edu>
2021-12-12 13:54:52 -0600

    corrected Die props

--- week3/Assignment1/rolldice/src/Die.js
+++ week3/Assignment1/rolldice/src/Die.js
@@ -1,13 +1,13 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix, faDice} from '@fortawesome/free-solid-svg-icons';
 
-function Die(props) {
-    if (props.value === 1) return <FontAwesomeIcon icon ={faDiceOne} />
-    else if (props.value === 2) return <FontAwesomeIcon icon ={faDiceTwo} />
-    else if (props.value === 3) return <FontAwesomeIcon icon ={faDiceThree} />
-    else if (props.value === 4) return <FontAwesomeIcon icon ={faDiceFour} />
-    else if (props.value === 5) return <FontAwesomeIcon icon ={faDiceFive} />
-    else if (props.value === 6) return <FontAwesomeIcon icon ={faDiceSix} />
+function Die({value}) {
+    if (value === 1) return <FontAwesomeIcon icon ={faDiceOne} />
+    else if (value === 2) return <FontAwesomeIcon icon ={faDiceTwo} />
+    else if (value === 3) return <FontAwesomeIcon icon ={faDiceThree} />
+    else if (value === 4) return <FontAwesomeIcon icon ={faDiceFour} />
+    else if (value === 5) return <FontAwesomeIcon icon ={faDiceFive} />
+    else if (value === 6) return <FontAwesomeIcon icon ={faDiceSix} />
     else return <FontAwesomeIcon icon ={faDice} />
 }
 
