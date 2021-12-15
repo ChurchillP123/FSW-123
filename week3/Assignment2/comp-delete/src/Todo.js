2146259a6354c04c7db933ec254cd9fb1b77c7fb
Churchill perry <churchill.perry@bryanuniversity.edu>
2021-12-14 23:55:09 -0600

    added line-through for completed todos

--- week3/Assignment2/comp-delete/src/Todo.js
+++ week3/Assignment2/comp-delete/src/Todo.js
@@ -2,7 +2,7 @@ function Todo({todo, completeTodo, deleteTodo}) {
     return (
         <li key ={todo.id}>
             <input type="checkbox" checked ={todo.isCompleted} onChange ={() => completeTodo(todo.id)}/>
-            {todo.text}
+            <span style ={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>{todo.text}</span>
             <button onClick ={() => deleteTodo(todo.id)}>X</button>
         </li>
     );
